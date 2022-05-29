d46d79ec5299ec6e34766d3973c853ce04ca7b88
syafiq_alaudin <syafiq@fynenz.com>
2022-05-29 23:21:24 +0700

    fixing some feature

--- client/main.js
+++ client/main.js
@@ -4,8 +4,6 @@ submit.addEventListener('click', function(e){
     e.preventDefault()
     var discount=document.getElementById('discount').value
     var price=document.getElementById('price').value
-    console.log(discount)
-    console.log(price)
 
     fetch('http://localhost:4000/api/discount', {
         method: 'POST',
@@ -19,6 +17,6 @@ submit.addEventListener('click', function(e){
     }).then(function(response){ 
         return response.json()
     }).then(function(data){
-        console.log(data)
-    }).catch(error => console.error('Error:', error)); 
+        document.getElementById("result").innerHTML = data.price.price
+    }).catch(() => document.getElementById("result").innerHTML = '0'); 
 })
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.post('/api/discount', (req,res)=> {
     const discountPercent = Calc.Percent(req.body.discount)
     const totalDiscount = Calc.Discount(discountPercent,req.body.price)
     const price = Calc.PriceAfterDiscount(totalDiscount,req.body.price)
-    if(req.body.discount&&req.body.price){
+    if(req.body.discount&&req.body.price&&req.body.discount!=0&&req.body.price!=0){
         return res.send({
             code: 200,
             status: 'SUCCESS',
